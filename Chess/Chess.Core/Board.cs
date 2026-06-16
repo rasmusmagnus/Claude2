7a198bb81fd5d804870e566c3c94974e4119f2dd
Frederik Bang-Jensen <fbj@danskecommodities.com>
2026-06-16 20:03:24 +0200

    init board update

--- Chess/Chess.Core/Board.cs
+++ Chess/Chess.Core/Board.cs
@@ -8,10 +8,31 @@ public class Board
     private readonly IEventProducer<IGameEvent> _producer;
     private readonly IEventConsumer<IGameEvent> _consumer;
 
+    public static string StartBoardFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    
     public Board(IEventProducer<IGameEvent> producer, IEventConsumer<IGameEvent> consumer)
     {
         _producer = producer;
         _consumer = consumer;
+    }
+
+    private Board(string producer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Board GetBoardFromFen(string fen)
+    {
+        return new Board(fen);
+    }
+
+    public void MakeMove(IChessMove move)
+    {
+    }
+
+    public static string ToFen(Board board)
+    {
+        return StartBoardFen;
     }
 
     public async Task RunAsync(CancellationToken token)
